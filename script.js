1afad56e7ed7cee5f762201f99ebef3d9cfdace7
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-07-14 22:37:27 -0300

    atualização

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const buttonSubmit = document.querySelector("#buttonDeposito");
+const artigoDeposito = document.querySelector(".Deposito");
+const artigoTransferencia = document.querySelector(".Transferencia");
+const artigoFormulario = document.querySelector(".Formulario");
 /*
 const buttonSaque = document.querySelector("#buttonSaque");
 const buttonSaldo = document.querySelector("#buttonSaldo");
 */
+function ocult(){
+artigoDeposito.className="ocultar";
+artigoTransferencia.className="ocultar";
+// artigoFormulario.className="ocultar";
 
+}
+ocult();
 buttonSubmit.addEventListener("click", function(event) {
 event.preventDefault();
 let nome = document.getElementById("nomeInput");
@@ -14,6 +23,7 @@ document.querySelector("nome").innerHTML = `Nome: ${nome}`;
 document.querySelector(".sobreNomeInput").innerHTML = `SobreNome ${sobreNome}`;
 document.querySelector(".depositoBancario").innerHTML = `Valor Depositado R$ ${deposito}`;
 console.log(nome,sobreNome,deposito);
+
 
 
 });
